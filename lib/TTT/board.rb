508e7c36eac945a227f20e1f4e509a160a4ae1b2
LBKinson <laura.kinson@gmail.com>
2015-04-22 22:46:36 -0700

    added board initialize tests

--- lib/TTT/board.rb
+++ lib/TTT/board.rb
@@ -1,5 +1,16 @@
 module TTT
 	class Board
+		def initialize
+			:grid
+		end
+
+		def grid
+			Array.new(9)
+		end
+
+		def row
+			Array.new(3)
+		end
 	end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,24 @@ module TTT
 				board = Board.new
 				expect(board)
 			end
+
+			it "initializes with a grid" do
+				expect{ Board.new(grid: "grid") }
+			end
+
+			it "has a grid system of 9 spaces" do
+				board = Board.new
+				expect(board.grid.size).to eq(9)
+			end
+
+			it "has a board system with default 3 rows" do
+				board = Board.new
+				expect(board.row.size).to eq(3)
+			end
+
+			it ""
+
+
 		end
 
 	end
